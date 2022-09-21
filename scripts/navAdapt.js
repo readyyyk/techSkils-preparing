b47c4e0f5ba35269d432bdb682c401b6a7c72be8
readyyyk <mr.sairexer@gmail.com>
2022-09-21 15:30:43 +0300

    nav done

--- scripts/navAdapt.js
+++ scripts/navAdapt.js
@@ -1,10 +1,37 @@
-if(window.innerWidth <= 910){
-    let navEls = document.querySelectorAll(".nav")
-    navEls.forEach(el => el.remove())
-}
-window.onresize = (e) => {
+
+let mn = document.querySelector("#mobile__nav"),
+    clBtn = document.querySelector("#mobile__nav__close"),
+    opBtn = document.querySelector("#mobile__nav__open")
+    navEls = document.querySelectorAll(".nav-desc")
+
+resHandler()
+window.addEventListener("resize", resHandler)
+
+function resHandler(){
     if(window.innerWidth <= 910){
-        let navEls = document.querySelectorAll(".nav")
         navEls.forEach(el => el.remove())
+        mn.style.display = "flex"
+        clBtn.style.display = "block"
+        opBtn.style.display = "block"
+    } else {
+        mn.style.display = "none"
+        clBtn.style.display = "none"
+        opBtn.style.display = "none"
     }
 }
+    
+    clBtn.addEventListener("click", ()=>{
+        opBtn.style.opacity = "1"
+        clBtn.style.zIndex = "2"
+        mn.style.left="-100%"
+        setTimeout(()=>{
+            mn.style.opacity="0"
+        },200)
+    })
+
+    opBtn.addEventListener("click", ()=>{
+        clBtn.style.zIndex = "0"
+        mn.style.left="0"
+        mn.style.opacity="1"
+        opBtn.style.opacity = "0"
+    })
